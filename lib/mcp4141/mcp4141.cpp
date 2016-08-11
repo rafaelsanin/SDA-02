d08f25c97a76776924c88bfef8549ab2b6054e55
Victor Camargo <vcamargo.e@gmail.com>
2016-08-10 20:50:55 -0500

    03- Implementación de las funciones
    1- Se implementaron las funciones de la clase
    2- Se implemento un Script que muestra y permite evaluar las funcionalidades de la clase
    3- Se agrego el archivo UML.dia
    
     Changes to be committed:
    	new file:   lib/mcp4141/docs/UML4141.dia
    	modified:   lib/mcp4141/mcp4141.cpp
    	modified:   src/main.ino

--- lib/mcp4141/mcp4141.cpp
+++ lib/mcp4141/mcp4141.cpp
@@ -1,1 +1,40 @@
 #include <mcp4141.h>
+
+MCP4141::MCP4141(uint8_t pin){
+  SPI.begin();
+  chip_select = pin;
+  position = 0;
+  pinMode(chip_select, OUTPUT);
+}
+
+uint8_t MCP4141::set(uint8_t val){
+  digitalWrite(chip_select, LOW);
+  SPI.transfer16(val);
+  digitalWrite(chip_select, HIGH);
+  position = val;
+  return position;
+}
+
+uint8_t MCP4141::getPosition(){
+  return position;
+}
+
+uint8_t MCP4141::increment(){
+  if(position < 127){
+    digitalWrite(chip_select, LOW);
+    SPI.transfer(4);
+    digitalWrite(chip_select, HIGH);
+    position+=1;
+  }
+  return position;
+}
+
+uint8_t MCP4141::decrement(){
+  if(position > 0){
+    digitalWrite(chip_select, LOW);
+    SPI.transfer(8);
+    digitalWrite(chip_select, HIGH);
+    position-=1;
+  }
+  return position;
+}
--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,15 @@
 #include <mcp4141.h>
 
-void setup(){
+MCP4141 digital_pot = MCP4141(10);
 
+void setup(){
+  Serial.begin(115200);
 }
 
 void loop(){
-
+  for(int i=0;i<130;i+=10){
+    digital_pot.set(i);
+    Serial.println(digital_pot.getPosition());
+    delay(1000);
+  }
 }
